821c0af55366d0cfcb54ede2c13f8e82070ed405
David Weber <dsw7@sfu.ca>
2023-01-19 23:58:52 -0800

    Add hello test

--- src/tests/test_commands.py
+++ src/tests/test_commands.py
@@ -1,6 +1,14 @@
 from pytest import mark
 from src.py.serial_connection import SerialConnection
 
+def test_command_hello(connection: SerialConnection) -> None:
+
+    connection.send_message('hello')
+    status, returned_msg = connection.receive_message()
+
+    assert status
+    assert returned_msg == 'Hello from InoDAQV2'
+
 PAIRS_PING = [
     ('ping', 'Built in LED is ON'),
     ('ping', 'Built in LED is OFF')
