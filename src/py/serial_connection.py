97c34eab198c1fbacec1fca0cdcd7d1c5e0fc508
David Weber <dsw7@sfu.ca>
2023-01-21 05:14:25 -0800

    Load configs from file

--- src/py/serial_connection.py
+++ src/py/serial_connection.py
@@ -2,25 +2,34 @@ import sys
 import logging
 from time import sleep
 from json import dumps
-from typing import TypeVar
-from typing import Tuple
-from typing import Dict
-from typing import Union
+from typing import TypeVar, Tuple
+from configparser import ConfigParser
+from pathlib import Path
 import serial
 
-BAUD_RATE = 9600
 T = TypeVar('T')
-ENCODING = 'utf-8'
+
+def read_ini() -> ConfigParser:
+
+    path_ini = Path(__file__).parent / 'inodaqv2.ini'
+
+    if not path_ini.exists():
+        sys.exit(f'Path "{path_ini}" does not exist')
+
+    configs = ConfigParser()
+    configs.read(path_ini)
+
+    return configs
 
 
 class SerialConnection:
 
-    def __init__(self: T, **cli_params: Dict[str, Union[str, bool]]) -> T:
+    def __init__(self: T) -> T:
 
+        self.configs = read_ini()
         self.serial_port_obj = None
-        self.port = cli_params['port']
 
-        if cli_params['debug']:
+        if self.configs['logging'].getboolean('debug'):
             level = logging.DEBUG
         else:
             level = logging.INFO
@@ -37,15 +46,15 @@ class SerialConnection:
     def __enter__(self: T) -> T:
 
         connection_params = {
-            'baudrate': BAUD_RATE,
+            'baudrate': self.configs['connection']['baud'],
             'parity': serial.PARITY_NONE,
             'stopbits': serial.STOPBITS_ONE,
             'bytesize': serial.EIGHTBITS,
             'timeout': 5,
-            'port': self.port
+            'port': self.configs['connection']['port']
         }
 
-        self.logger.debug('Connecting using parameters:\n%s', dumps(connection_params, indent=4))
+        self.logger.info('Connecting using parameters:\n%s', dumps(connection_params, indent=4))
 
         try:
             self.serial_port_obj = serial.Serial(**connection_params)
@@ -66,10 +75,10 @@ class SerialConnection:
     def __exit__(self: T, *args) -> None:
 
         if self.serial_port_obj is None:
-            self.logger.debug('Not closing connection. Connection was never opened!')
+            self.logger.info('Not closing connection. Connection was never opened!')
             return
 
-        self.logger.debug('Closing connection!')
+        self.logger.info('Closing connection!')
 
         if self.serial_port_obj.is_open:
 
@@ -80,7 +89,7 @@ class SerialConnection:
     def send_message(self: T, message: str) -> None:
 
         self.logger.debug('Sending message: "%s"', message)
-        message = message.encode(encoding=ENCODING)
+        message = message.encode(encoding=self.configs['connection']['encoding'])
 
         self.logger.debug('Sent %i bytes', self.serial_port_obj.write(message))
         self.serial_port_obj.flush()
@@ -105,7 +114,7 @@ class SerialConnection:
             self.logger.debug('Received message: %s', bytes_from_dev)
 
         try:
-            results = bytes_from_dev.decode(ENCODING).strip()
+            results = bytes_from_dev.decode(self.configs['connection']['encoding']).strip()
         except UnicodeDecodeError as e:
             return False, f'An exception occurred when decoding results: "{e}"'
 
