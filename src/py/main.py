139887441aa592bd047f89e144687bb19e631973
David Weber <dsw7@sfu.ca>
2023-01-22 05:20:17 -0800

    Replace curses with tkinter

--- src/py/main.py
+++ src/py/main.py
@@ -1,98 +1,63 @@
-import sys
 import typing
-import curses
-from dataclasses import dataclass
-from serial_connection import SerialConnection
+import tkinter as tk
 
 T = typing.TypeVar('T')
-MIN_COLUMNS = 150
-MIN_ROWS = 25
-
-@dataclass
-class C:
-
-    columns: int = 0
-    rows: int = 0
-
-    col_a: int = 1
-    col_b: int = 11
-    col_c: int = 21
 
 
-class MainPanel:
+class PanelDig:
 
-    def __init__(self: T, stdscr: curses.window, connection: SerialConnection) -> T:
+    def __init__(self: T, master: tk.Tk) -> T:
 
-        self.stdscr = stdscr
-        self.connection = connection
-        self.position = 3
-        self.toggled = False
+        frame = tk.LabelFrame(master, relief=tk.GROOVE, bd=1, text='Toggle digital pins')
+        frame.pack(side='left', fill='both', expand=True, padx=5, pady=5)
 
-    def cursor_up(self: T) -> None:
-        self.position -= 1
+        self.pins = {}
 
-        if self.position < 3:
-            self.position = 14
+        for p in range(2, 14):
+            pin = f'Pin {p}'
+            self.pins[pin] = tk.BooleanVar()
 
-    def cursor_down(self: T) -> None:
-        self.position += 1
+            tk.Checkbutton(
+                frame, text=pin, variable=self.pins[pin], command=self.toggle
+            ).grid(column=1, sticky='W', row=p, padx=5)
 
-        if self.position > 14:
-            self.position = 3
-
-    def toggle_pin(self: T) -> None:
-        self.toggled = not self.toggled
-
-    def dig_command_panel(self: T) -> None:
-
-        self.stdscr.hline(0, 0, '-', C.columns)
-        self.stdscr.addstr(1, C.col_a, 'Toggle digital pins', curses.A_BOLD + curses.A_UNDERLINE)
-
-        for idx in range(3, 15):
-
-            if idx == self.position:
-                mode = curses.A_REVERSE
-            else:
-                mode = curses.A_NORMAL
-
-            self.stdscr.addstr(idx, C.col_a, f'Pin {idx - 1}', mode)
-            self.stdscr.addstr(idx, C.col_b, '[ ]', mode)
-
-    def event_loop(self: T) -> None:
-
-        self.dig_command_panel()
-        key_input = None
-
-        while key_input != ord('q'):
-            key_input = self.stdscr.getch()
-
-            if key_input == curses.KEY_UP:
-                self.cursor_up()
-            elif key_input == curses.KEY_DOWN:
-                self.cursor_down()
-            elif key_input == curses.KEY_ENTER:
-                self.toggle_pin()
-
-            self.dig_command_panel()
-            self.stdscr.refresh()
+    def toggle(self: T) -> None:
+        print(self.pins)
 
 
-def main(stdscr: curses.window) -> None:
+class PanelPWM:
 
-    curses.curs_set(0)
-    rows, columns = stdscr.getmaxyx()
+    def __init__(self: T, master: tk.Tk) -> T:
 
-    if columns < MIN_COLUMNS:
-        sys.exit('Terminal is not wide enough!')
+        frame = tk.LabelFrame(master, relief=tk.GROOVE, bd=1, text='Replace')
+        frame.pack(side='left', fill='both', expand=True, padx=5, pady=5)
 
-    if rows < MIN_ROWS:
-        sys.exit('Terminal is not tall enough!')
+        tk.Label(frame, text='Enter ID here:').pack(anchor='w', padx=5)
+        self._id = tk.Entry(frame)
+        self._id.pack(fill='x', padx=5)
+        self._id.insert(tk.END, '123')
 
-    C.columns = columns
-    C.rows = rows
+        tk.Button(frame, text='Replace', command=self.button_replace_callback).pack(
+            fill='x', side='bottom', padx=5, pady=5
+        )
 
-    with SerialConnection() as connection:
-        MainPanel(stdscr, connection).event_loop()
+    def button_replace_callback(self):
+        print('Entry ID:', self._id.get())
+
+
+class Master:
+    def __init__(self, master):
+        master.title('Unknown')
+        master.geometry('1200x300')
+
+        PanelDig(master)
+        PanelPWM(master)
+
+
+def main():
+    root = tk.Tk()
+    Master(root)
+    root.mainloop()
 
 if __name__ == '__main__':
-    curses.wrapper(main)
+    main()
