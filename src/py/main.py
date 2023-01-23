773124cfbab0f35dce1c242981260efc45e1772b
David Weber <dsw7@sfu.ca>
2023-01-22 22:47:42 -0800

    Add digital panel skeleton

--- src/py/main.py
+++ src/py/main.py
@@ -2,6 +2,8 @@ import typing
 import tkinter as tk
 
 T = typing.TypeVar('T')
+PACK_FRAME = {'side': 'left', 'fill': 'both', 'expand': True, 'padx': 5, 'pady': 5}
+PACK_GRID = {'column': 1, 'sticky': 'W', 'padx': 5}
 
 
 class PanelDig:
@@ -9,20 +11,28 @@ class PanelDig:
     def __init__(self: T, master: tk.Tk) -> T:
 
         frame = tk.LabelFrame(master, relief=tk.GROOVE, bd=1, text='Toggle digital pins')
-        frame.pack(side='left', fill='both', expand=True, padx=5, pady=5)
+        frame.pack(**PACK_FRAME)
 
         self.pins = {}
 
-        for p in range(2, 14):
-            pin = f'Pin {p}'
+        for pin in range(2, 14):
             self.pins[pin] = tk.BooleanVar()
 
-            tk.Checkbutton(
-                frame, text=pin, variable=self.pins[pin], command=self.toggle
-            ).grid(column=1, sticky='W', row=p, padx=5)
+        tk.Checkbutton(frame, text='Pin 2', variable=self.pins[2], command=lambda: self.toggle(2)).grid(**PACK_GRID, row=2)
+        tk.Checkbutton(frame, text='Pin 3', variable=self.pins[3], command=lambda: self.toggle(3)).grid(**PACK_GRID, row=3)
+        tk.Checkbutton(frame, text='Pin 4', variable=self.pins[4], command=lambda: self.toggle(4)).grid(**PACK_GRID, row=4)
+        tk.Checkbutton(frame, text='Pin 5', variable=self.pins[5], command=lambda: self.toggle(5)).grid(**PACK_GRID, row=5)
+        tk.Checkbutton(frame, text='Pin 6', variable=self.pins[6], command=lambda: self.toggle(6)).grid(**PACK_GRID, row=6)
+        tk.Checkbutton(frame, text='Pin 7', variable=self.pins[7], command=lambda: self.toggle(7)).grid(**PACK_GRID, row=7)
+        tk.Checkbutton(frame, text='Pin 8', variable=self.pins[8], command=lambda: self.toggle(8)).grid(**PACK_GRID, row=8)
+        tk.Checkbutton(frame, text='Pin 9', variable=self.pins[9], command=lambda: self.toggle(9)).grid(**PACK_GRID, row=9)
+        tk.Checkbutton(frame, text='Pin 10', variable=self.pins[10], command=lambda: self.toggle(10)).grid(**PACK_GRID, row=10)
+        tk.Checkbutton(frame, text='Pin 11', variable=self.pins[11], command=lambda: self.toggle(11)).grid(**PACK_GRID, row=11)
+        tk.Checkbutton(frame, text='Pin 12', variable=self.pins[12], command=lambda: self.toggle(12)).grid(**PACK_GRID, row=12)
+        tk.Checkbutton(frame, text='Pin 13', variable=self.pins[13], command=lambda: self.toggle(13)).grid(**PACK_GRID, row=13)
 
-    def toggle(self: T) -> None:
-        print(self.pins)
+    def toggle(self: T, pin: int) -> None:
+        print(pin, self.pins[pin].get())
 
 
 class PanelPWM:
@@ -30,7 +40,7 @@ class PanelPWM:
     def __init__(self: T, master: tk.Tk) -> T:
 
         frame = tk.LabelFrame(master, relief=tk.GROOVE, bd=1, text='Replace')
-        frame.pack(side='left', fill='both', expand=True, padx=5, pady=5)
+        frame.pack(**PACK_FRAME)
 
         tk.Label(frame, text='Enter ID here:').pack(anchor='w', padx=5)
         self._id = tk.Entry(frame)
