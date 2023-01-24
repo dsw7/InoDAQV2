7189a7a7ba4a002e8cc773f74371529f690042d2
David Weber <dsw7@sfu.ca>
2023-01-24 01:13:10 -0800

    Use ABC

--- src/py/user_interface.py
+++ src/py/user_interface.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import TypeVar
 from math import ceil
 from functools import partial
@@ -5,27 +6,45 @@ import tkinter as tk
 from serial_connection import SerialConnection
 
 T = TypeVar('T')
-PACK_FRAME = {'side': 'left', 'fill': 'both', 'expand': True, 'padx': 5, 'pady': 5, 'ipady': 3, 'ipadx': 3}
-DUTY_CYCLE_TO_ANALOG = 255 / 100
 
 
-class PanelDig:
+class PanelBase(ABC):
 
-    kw_grid = {'column': 1, 'sticky': 'W', 'padx': 3}
+    kw_labelframe = {
+        'side': 'left', 'fill': 'both', 'expand': True, 'padx': 5, 'pady': 5, 'ipady': 3, 'ipadx': 3
+    }
 
     def __init__(self: T, root: tk.Tk, connection: SerialConnection) -> T:
 
+        self.root = root
         self.connection = connection
         self.pins = {}
+        self.setup_panel()
 
-        frame = tk.LabelFrame(root, relief=tk.GROOVE, bd=1, text='Digital')
-        frame.pack(**PACK_FRAME)
+    @abstractmethod
+    def setup_panel(self: T) -> None:
+        pass
+
+    @abstractmethod
+    def toggle(self: T, pin: int) -> None:
+        pass
+
+
+class PanelDig(PanelBase):
+
+    kw_button = {'column': 1, 'sticky': 'W', 'padx': 3}
+
+    def setup_panel(self: T) -> None:
+
+        frame = tk.LabelFrame(self.root, relief=tk.GROOVE, bd=1, text='Digital')
+        frame.pack(**self.kw_labelframe)
 
         for p in range(2, 14):
             self.pins[p] = tk.BooleanVar()
-            tk.Checkbutton(frame, text=f'Pin {p}', variable=self.pins[p], command=partial(self.toggle, p)).grid(**self.kw_grid, row=p)
+            tk.Checkbutton(frame, text=f'Pin {p}', variable=self.pins[p], command=partial(self.toggle, p)).grid(**self.kw_button, row=p)
 
     def toggle(self: T, pin: int) -> None:
+
         command = f'dig:{pin}:'
 
         if self.pins[pin].get():
@@ -37,18 +56,16 @@ class PanelDig:
         self.connection.receive_message()
 
 
-class PanelPWM:
+class PanelPWM(PanelBase):
 
     kw_button = {'column': 1, 'padx': 3}
     kw_scale = {'column': 2, 'padx': 3}
+    duty_cycle_to_analog = 255 / 100
 
-    def __init__(self: T, root: tk.Tk, connection: SerialConnection) -> T:
+    def setup_panel(self: T) -> None:
 
-        self.connection = connection
-        self.pins = {}
-
-        frame = tk.LabelFrame(root, relief=tk.GROOVE, bd=1, text='PWM')
-        frame.pack(**PACK_FRAME)
+        frame = tk.LabelFrame(self.root, relief=tk.GROOVE, bd=1, text='PWM')
+        frame.pack(**self.kw_labelframe)
 
         for row, p in enumerate((3, 5, 6, 9, 10, 11), 2):
             self.pins[p] = tk.IntVar()
@@ -56,7 +73,8 @@ class PanelPWM:
             tk.Scale(frame, variable=self.pins[p], orient=tk.HORIZONTAL).grid(**self.kw_scale, row=row)
 
     def toggle(self: T, pin: int) -> None:
-        pwm = ceil(DUTY_CYCLE_TO_ANALOG * self.pins[pin].get())
+
+        pwm = ceil(self.duty_cycle_to_analog * self.pins[pin].get())
         command = f'pwm:{pin}:{pwm}'
 
         self.connection.send_message(command)
