253f2bbcfd8d1ba09458c4e0d184fb69ec0d21b4
David Weber <dsw7@sfu.ca>
2023-01-24 00:56:30 -0800

    Add PWM write panel

--- src/py/user_interface.py
+++ src/py/user_interface.py
@@ -1,37 +1,29 @@
 from typing import TypeVar
+from math import ceil
+from functools import partial
 import tkinter as tk
 from serial_connection import SerialConnection
 
 T = TypeVar('T')
-PACK_FRAME = {'side': 'left', 'fill': 'both', 'expand': True, 'padx': 5, 'pady': 5}
-PACK_GRID = {'column': 1, 'sticky': 'W', 'padx': 5}
+PACK_FRAME = {'side': 'left', 'fill': 'both', 'expand': True, 'padx': 5, 'pady': 5, 'ipady': 3, 'ipadx': 3}
+DUTY_CYCLE_TO_ANALOG = 255 / 100
 
 
 class PanelDig:
+
+    kw_grid = {'column': 1, 'sticky': 'W', 'padx': 3}
 
     def __init__(self: T, root: tk.Tk, connection: SerialConnection) -> T:
 
         self.connection = connection
         self.pins = {}
 
-        frame = tk.LabelFrame(root, relief=tk.GROOVE, bd=1, text='Toggle digital pins')
+        frame = tk.LabelFrame(root, relief=tk.GROOVE, bd=1, text='Digital')
         frame.pack(**PACK_FRAME)
 
-        for pin in range(2, 14):
-            self.pins[pin] = tk.BooleanVar()
-
-        tk.Checkbutton(frame, text='Pin 2', variable=self.pins[2], command=lambda: self.toggle(2)).grid(**PACK_GRID, row=2)
-        tk.Checkbutton(frame, text='Pin 3', variable=self.pins[3], command=lambda: self.toggle(3)).grid(**PACK_GRID, row=3)
-        tk.Checkbutton(frame, text='Pin 4', variable=self.pins[4], command=lambda: self.toggle(4)).grid(**PACK_GRID, row=4)
-        tk.Checkbutton(frame, text='Pin 5', variable=self.pins[5], command=lambda: self.toggle(5)).grid(**PACK_GRID, row=5)
-        tk.Checkbutton(frame, text='Pin 6', variable=self.pins[6], command=lambda: self.toggle(6)).grid(**PACK_GRID, row=6)
-        tk.Checkbutton(frame, text='Pin 7', variable=self.pins[7], command=lambda: self.toggle(7)).grid(**PACK_GRID, row=7)
-        tk.Checkbutton(frame, text='Pin 8', variable=self.pins[8], command=lambda: self.toggle(8)).grid(**PACK_GRID, row=8)
-        tk.Checkbutton(frame, text='Pin 9', variable=self.pins[9], command=lambda: self.toggle(9)).grid(**PACK_GRID, row=9)
-        tk.Checkbutton(frame, text='Pin 10', variable=self.pins[10], command=lambda: self.toggle(10)).grid(**PACK_GRID, row=10)
-        tk.Checkbutton(frame, text='Pin 11', variable=self.pins[11], command=lambda: self.toggle(11)).grid(**PACK_GRID, row=11)
-        tk.Checkbutton(frame, text='Pin 12', variable=self.pins[12], command=lambda: self.toggle(12)).grid(**PACK_GRID, row=12)
-        tk.Checkbutton(frame, text='Pin 13', variable=self.pins[13], command=lambda: self.toggle(13)).grid(**PACK_GRID, row=13)
+        for p in range(2, 14):
+            self.pins[p] = tk.BooleanVar()
+            tk.Checkbutton(frame, text=f'Pin {p}', variable=self.pins[p], command=partial(self.toggle, p)).grid(**self.kw_grid, row=p)
 
     def toggle(self: T, pin: int) -> None:
         command = f'dig:{pin}:'
@@ -47,19 +39,25 @@ class PanelDig:
 
 class PanelPWM:
 
-    def __init__(self: T, root: tk.Tk) -> T:
+    kw_button = {'column': 1, 'padx': 3}
+    kw_scale = {'column': 2, 'padx': 3}
 
-        frame = tk.LabelFrame(root, relief=tk.GROOVE, bd=1, text='Replace')
+    def __init__(self: T, root: tk.Tk, connection: SerialConnection) -> T:
+
+        self.connection = connection
+        self.pins = {}
+
+        frame = tk.LabelFrame(root, relief=tk.GROOVE, bd=1, text='PWM')
         frame.pack(**PACK_FRAME)
 
-        tk.Label(frame, text='Enter ID here:').pack(anchor='w', padx=5)
-        self._id = tk.Entry(frame)
-        self._id.pack(fill='x', padx=5)
-        self._id.insert(tk.END, '123')
+        for row, p in enumerate((3, 5, 6, 9, 10, 11), 2):
+            self.pins[p] = tk.IntVar()
+            tk.Button(frame, text=p, command=partial(self.toggle, p), width=3).grid(**self.kw_button, row=row)
+            tk.Scale(frame, variable=self.pins[p], orient=tk.HORIZONTAL).grid(**self.kw_scale, row=row)
 
-        tk.Button(frame, text='Replace', command=self.button_replace_callback).pack(
-            fill='x', side='bottom', padx=5, pady=5
-        )
+    def toggle(self: T, pin: int) -> None:
+        pwm = ceil(DUTY_CYCLE_TO_ANALOG * self.pins[pin].get())
+        command = f'pwm:{pin}:{pwm}'
 
-    def button_replace_callback(self):
-        print('Entry ID:', self._id.get())
+        self.connection.send_message(command)
+        self.connection.receive_message()
