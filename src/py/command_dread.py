3f5272725c5d99b02f1bbf44a044cf936fa62034
David Weber <dsw7@sfu.ca>
2023-02-06 00:38:17 -0800

    Add dread code

---
+++ src/py/command_dread.py
@@ -0,0 +1,38 @@
+from tkinter import LabelFrame, Text, Button
+from tkinter import GROOVE, END
+from base import PanelBase, T
+
+
+class PanelDread(PanelBase):
+
+    kw_label_text = {'column': 1, 'padx': 3, 'pady': 3}
+    kw_button = {'column': 1, 'pady': 3, 'row': 6}
+
+    def setup_panel(self: T) -> None:
+
+        frame = LabelFrame(self.root, relief=GROOVE, bd=1, text='DigitalRead')
+        frame.pack(**self.kw_labelframe)
+
+        for p in range(0, 6):
+            subframe = LabelFrame(frame, relief=GROOVE, bd=1, text=f'A{p}')
+            subframe.grid(**self.kw_label_text, row=p)
+
+            self.pins[p] = Text(subframe, height=1, width=20)
+            self.pins[p].grid(**self.kw_label_text, row=p)
+
+            self.pins[p].insert(END, ' ...')
+
+        Button(frame, text='READ', command=self.toggle).grid(**self.kw_button)
+
+    def toggle(self: T) -> None:
+
+        self.connection.send_message('dread')
+        status, message = self.connection.receive_message()
+
+        if not status:
+            self.logger.error(message)
+            return
+
+        for p, state in enumerate(message.split(',')):
+            self.pins[p].delete(1.0, END)
+            self.pins[p].insert(END, state)
