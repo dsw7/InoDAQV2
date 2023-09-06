0bedc95854050b7e298e3ee3062534ae64d6f8d0
David Weber <dsw7@sfu.ca>
2023-09-06 01:04:21 -0700

    Format + profile dread

--- src/py/command_dread.py
+++ src/py/command_dread.py
@@ -1,26 +1,33 @@
-from tkinter import LabelFrame, Text, Button
-from tkinter import GROOVE, END
-from base import PanelBase
+from logging import getLogger
+from tkinter import Tk, LabelFrame, Text, Button, GROOVE, END
+from serial_connection import SerialConnection
 
 
-class PanelDread(PanelBase):
-    kw_label_text = {"column": 1, "padx": 3, "pady": 3}
-    kw_button = {"column": 1, "pady": 3, "row": 6}
+class PanelDread:
+    logger = getLogger("inodaqv2")
+
+    def __init__(self, root: Tk, connection: SerialConnection) -> None:
+        self.root = root
+        self.connection = connection
+        self.pins: dict[int, Text] = {}
+        self.setup_panel()
 
     def setup_panel(self) -> None:
         frame = LabelFrame(self.root, relief=GROOVE, bd=1, text="DigitalRead")
-        frame.pack(**self.kw_labelframe)
+        frame.pack(
+            side="left", fill="both", expand=True, padx=5, pady=5, ipady=3, ipadx=3
+        )
 
         for p in range(0, 6):
             subframe = LabelFrame(frame, relief=GROOVE, bd=1, text=f"A{p}")
-            subframe.grid(**self.kw_label_text, row=p)
+            subframe.grid(column=1, padx=3, pady=3, row=p)
 
             self.pins[p] = Text(subframe, height=1, width=20)
-            self.pins[p].grid(**self.kw_label_text, row=p)
+            self.pins[p].grid(column=1, padx=3, pady=3, row=p)
 
             self.pins[p].insert(END, " ...")
 
-        Button(frame, text="READ", command=self.toggle).grid(**self.kw_button)
+        Button(frame, text="READ", command=self.toggle).grid(column=1, pady=3, row=6)
 
     def toggle(self) -> None:
         self.connection.send_message("dread")
