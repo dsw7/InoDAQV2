6d2abe373eb31def61b8b69a36cc5e94415758b6
David Weber <dsw7@sfu.ca>
2023-09-06 01:17:17 -0700

    Provide generator type hint

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 from configparser import ConfigParser
+from typing import Generator
 from pytest import fixture
 from src.py.serial_connection import SerialConnection
 
 
 @fixture(scope="session")
-def connection() -> SerialConnection:
+def connection() -> Generator[SerialConnection, None, None]:
     path_ini = Path(__file__).parents[1] / "configs/inodaqv2.ini"
 
     configs = ConfigParser()
