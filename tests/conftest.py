2b97323007730a9420c8c2d0b5f2aaa814f6f930
David Weber <dsw7@sfu.ca>
2023-09-06 02:57:43 -0700

    Fix test

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from inodaqv2.serial_connection import SerialConnection
 
 @fixture(scope="session")
 def connection() -> Generator[SerialConnection, None, None]:
-    path_ini = Path(__file__).parents[1] / "configs/inodaqv2.ini"
+    path_ini = Path.home() / ".inodaqv2.ini"
 
     configs = ConfigParser()
     configs.read(path_ini)
