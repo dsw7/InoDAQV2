016285fac7ec751e755a26532ed335abaddac7b4
dsw7 <dds3443@hotmail.com>
2023-10-01 03:18:21 -0700

    Add CSS

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="InoDAQV2",
     version="1.1",
-    package_data={"inodaqv2": ["templates/*", "static/*js"]},
+    package_data={"inodaqv2": ["templates/*", "static/*js", "static/*css"]},
     packages=find_packages(),
     install_requires=[
         "click",
