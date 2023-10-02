fdf5206be7416d989416a568e8fc725e9734a20c
dsw7 <dds3443@hotmail.com>
2023-10-02 02:33:34 -0700

    Simplify dig payload

--- tests/test_command_dig.py
+++ tests/test_command_dig.py
@@ -26,39 +26,34 @@ def test_command_dig_1(connection: InoIO, command: str, expected_msg: str) -> No
 
 
 PAIRS_DIG_2 = [
-    ("dig:2:on", "Pin 2 set to on"),
-    ("dig:3:on", "Pin 3 set to on"),
-    ("dig:4:on", "Pin 4 set to on"),
-    ("dig:5:on", "Pin 5 set to on"),
-    ("dig:6:on", "Pin 6 set to on"),
-    ("dig:7:on", "Pin 7 set to on"),
-    ("dig:8:on", "Pin 8 set to on"),
-    ("dig:9:on", "Pin 9 set to on"),
-    ("dig:10:on", "Pin 10 set to on"),
-    ("dig:11:on", "Pin 11 set to on"),
-    ("dig:12:on", "Pin 12 set to on"),
-    ("dig:13:on", "Pin 13 set to on"),
-    ("dig:2:off", "Pin 2 set to off"),
-    ("dig:3:off", "Pin 3 set to off"),
-    ("dig:4:off", "Pin 4 set to off"),
-    ("dig:5:off", "Pin 5 set to off"),
-    ("dig:6:off", "Pin 6 set to off"),
-    ("dig:7:off", "Pin 7 set to off"),
-    ("dig:8:off", "Pin 8 set to off"),
-    ("dig:9:off", "Pin 9 set to off"),
-    ("dig:10:off", "Pin 10 set to off"),
-    ("dig:11:off", "Pin 11 set to off"),
-    ("dig:12:off", "Pin 12 set to off"),
-    ("dig:13:off", "Pin 13 set to off"),
+    ("dig:2:on", "1;2,on"),
+    ("dig:3:on", "1;3,on"),
+    ("dig:4:on", "1;4,on"),
+    ("dig:5:on", "1;5,on"),
+    ("dig:6:on", "1;6,on"),
+    ("dig:7:on", "1;7,on"),
+    ("dig:8:on", "1;8,on"),
+    ("dig:9:on", "1;9,on"),
+    ("dig:10:on", "1;10,on"),
+    ("dig:11:on", "1;11,on"),
+    ("dig:12:on", "1;12,on"),
+    ("dig:13:on", "1;13,on"),
+    ("dig:2:off", "1;2,off"),
+    ("dig:3:off", "1;3,off"),
+    ("dig:4:off", "1;4,off"),
+    ("dig:5:off", "1;5,off"),
+    ("dig:6:off", "1;6,off"),
+    ("dig:7:off", "1;7,off"),
+    ("dig:8:off", "1;8,off"),
+    ("dig:9:off", "1;9,off"),
+    ("dig:10:off", "1;10,off"),
+    ("dig:11:off", "1;11,off"),
+    ("dig:12:off", "1;12,off"),
+    ("dig:13:off", "1;13,off"),
 ]
 
 
 @mark.parametrize("command, expected_msg", PAIRS_DIG_2)
 def test_command_dig_2(connection: InoIO, command: str, expected_msg: str) -> None:
     connection.write(command)
-
-    msg = connection.read()
-    status, returned_msg = msg.split(";")
-
-    assert int(status) == 1
-    assert returned_msg == expected_msg
+    assert expected_msg == connection.read()
