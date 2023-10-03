7ff2576d7162a99017179e98345b885263fae1bb
dsw7 <dds3443@hotmail.com>
2023-10-03 00:30:25 -0700

    Add digital read regex tests

--- tests/test_command_dread.py
+++ tests/test_command_dread.py
@@ -1,12 +1,23 @@
+from re import match
 from inoio import InoIO
+from pytest import mark
+from inodaqv2.components.actions import PAT_VALID_DREAD
 
 
 def test_command_dread(connection: InoIO) -> None:
     connection.write("dread")
-    msg = connection.read()
+    assert match(PAT_VALID_DREAD, connection.read()) is not None
 
-    status, values = msg.split(";")
-    assert int(status) == 1
 
-    for val in values.split(","):
-        assert 0 <= int(val) <= 1
+INVALID_REPLIES = [
+    "0;0,0,0,0,0,0",
+    "1;0,0,0,0,0,",
+    "1;0,0,0,0,0,10",
+    "1;a,b,0,0,0,1",
+    "1;0,0,0,0,0,0,0",
+]
+
+
+@mark.parametrize("reply", INVALID_REPLIES)
+def test_regex_invalid_reply(reply: str) -> None:
+    assert match(PAT_VALID_DREAD, reply) is None
