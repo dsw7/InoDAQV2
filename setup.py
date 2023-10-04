87d75382a056ea0d0586ee6898603288615798a2
dsw7 <dds3443@hotmail.com>
2023-10-04 00:14:21 -0700

    Package C++

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="InoDAQV2",
     version="1.1",
-    package_data={"inodaqv2": ["templates/*", "static/*js", "static/*css"]},
+    package_data={"inodaqv2": ["templates/*", "static/*js", "static/*css", "ino/*"]},
     packages=find_packages(),
     install_requires=[
         "click",
