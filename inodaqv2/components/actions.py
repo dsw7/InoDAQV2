0616824905dd40967217226307a1ddf0030717a3
dsw7 <dds3443@hotmail.com>
2023-10-19 01:38:51 -0700

    Simplify action

--- inodaqv2/components/actions.py
+++ inodaqv2/components/actions.py
@@ -49,14 +49,6 @@ TYPE_PAYLOAD_PWM = TypedDict(
         "pwm": str,
     },
 )
-TYPE_PAYLOAD_TONE = TypedDict(
-    "TYPE_PAYLOAD_TONE",
-    {
-        "rv": bool,
-        "pin": str,
-    },
-)
-
 PAT_VALID_DIG = re.compile(r"^1;\d{1,2},(on|off)$")
 PAT_VALID_PWM = re.compile(r"^1;\d{1,2},\d{1,3}$")
 PAT_VALID_AREAD = re.compile(r"^1;\d{1,4},\d{1,4},\d{1,4},\d{1,4},\d{1,4},\d{1,4}$")
@@ -202,24 +194,27 @@ def read_digital_pins() -> Union[TYPE_PAYLOAD_DREAD, dict[str, bool]]:
     }
 
 
-def set_tone(pin: str, frequency: str) -> TYPE_PAYLOAD_TONE:
+def set_tone(pin: str, frequency: str) -> dict[str, bool]:
+    if not frequency.isnumeric():
+        LOGGER.exception("Cannot convert '%s' to a frequency", frequency)
+        return {"rv": False}
+
     pin_id = pin.split("-")[1]
 
     command = f"tone:{pin_id}:{frequency}"
-
     LOGGER.info('Sending command: "%s"', command)
 
     try:
         conn.write(command)
     except errors.InoIOTransmissionError:
         LOGGER.exception("Failed to send command")
-        return {"rv": False, "pin": pin_id}
+        return {"rv": False}
 
     reply = conn.read()
     LOGGER.info('Received reply: "%s"', reply)
 
     if re.match(PAT_VALID_TONE, reply) is None:
         LOGGER.exception("Could not parse message. Reply is likely garbled")
-        return {"rv": False, "pin": pin_id}
+        return {"rv": False}
 
-    return {"rv": True, "pin": pin_id}
+    return {"rv": True}
