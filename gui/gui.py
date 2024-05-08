8182051a102a9bdbc2de476db191717fee0c15d4
David Weber <dds3443@hotmail.com>
2024-05-08 03:02:09 -0700

    Placeholder frames

--- gui/gui.py
+++ gui/gui.py
@@ -8,14 +8,49 @@ except _tkinter.TclError as exception:
     sys.exit(f'Missing X11 graphic layer: "{exception}"')
 
 
+def panel_dig() -> None:
+    frame = ttk.LabelFrame(root, padding=10, text="Digital")
+    frame.grid(row=0, column=0)
+
+    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+
+def panel_pwm() -> None:
+    frame = ttk.LabelFrame(root, padding=10, text="PWM")
+    frame.grid(row=0, column=1)
+
+    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+
+def panel_analog_read() -> None:
+    frame = ttk.LabelFrame(root, padding=10, text="Analog Read")
+    frame.grid(row=0, column=2)
+
+    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+
+def panel_digital_read() -> None:
+    frame = ttk.LabelFrame(root, padding=10, text="Digital Read")
+    frame.grid(row=0, column=3)
+
+    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+
+def panel_tone() -> None:
+    frame = ttk.LabelFrame(root, padding=10, text="Tone")
+    frame.grid(row=0, column=4)
+
+    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+
 def main() -> None:
     root.title("InoDAQV2")
 
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-
-    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    panel_dig()
+    panel_pwm()
+    panel_analog_read()
+    panel_digital_read()
+    panel_tone()
 
     root.mainloop()
 
